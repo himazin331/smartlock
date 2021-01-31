1aa84e11106cacac6de254a6ca6b181d785d739c
himazin331 <sion20022013@gmail.com>
2021-02-01 04:41:41 +0900

    T-camera ESP32 program

---
+++ esp32/sketch/app_httpd.cpp
@@ -0,0 +1,193 @@
+#include "esp_http_server.h"    // httpサーバ関連のヘッダーファイル
+#include "esp_timer.h"
+#include "esp_camera.h"         // カメラ関連のヘッダーファイル
+#include "img_converters.h"
+#include "camera_index.h"       // カメラのヘッダーファイル
+#include "Arduino.h"
+
+#define AS312_PIN 33    // 人感センサー ピン
+
+// 映像取得 レスポンスヘッダ情報
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char *_STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char *_STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+// httpdハンドラ
+httpd_handle_t stream_httpd = NULL;
+httpd_handle_t camera_httpd = NULL;
+
+// 画像取得
+static esp_err_t index_handler(httpd_req_t *req)
+{
+    camera_fb_t *fb = NULL; // フレーム
+    esp_err_t res = ESP_OK; // 成功(ESP_OK)/失敗(ESP_FAIL)フラグ
+
+    fb = esp_camera_fb_get(); // 画像取得
+
+    // 取得失敗 -> Error 500
+    if (!fb) {
+        httpd_resp_send_500(req);
+        return ESP_FAIL;
+    }
+
+    // レスポンスヘッダ
+    httpd_resp_set_type(req, "image/jpeg");
+    httpd_resp_set_hdr(req, "Content-Disposition", "inline; filename=capture.jpg");
+    // レスポンスデータ(画像)
+    res = httpd_resp_send(req, (const char *)fb->buf, fb->len);
+
+    // リソース解放
+    esp_camera_fb_return(fb);
+
+    return res;
+}
+
+// 映像取得
+static esp_err_t stream_handler(httpd_req_t *req)
+{
+    camera_fb_t *fb = NULL; // フレーム
+    esp_err_t res = ESP_OK; // 成功(ESP_OK)/失敗(ESP_FAIL)フラグ
+
+    // バッファ
+    size_t _jpg_buf_len = 0;
+    uint8_t *_jpg_buf = NULL;
+    char *part_buf[64];
+
+    static int64_t last_frame = 0; // 前回のフレーム送信終了時刻
+    if (!last_frame) {
+        last_frame = esp_timer_get_time(); // 処理時間 計測開始
+    }
+
+    // レスポンスヘッダ
+    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+    // セット失敗 -> 中断
+    if (res != ESP_OK) {
+        return res;
+    }
+
+    // 撮影
+    while (true) {
+        fb = esp_camera_fb_get(); // フレーム取得
+        // 取得失敗 -> 中断
+        if (!fb) {
+            Serial.printf("Camera capture failed");
+            res = ESP_FAIL;
+        } else {
+            _jpg_buf_len = fb->len;
+            _jpg_buf = fb->buf;
+        }
+
+        // レスポンスデータ
+        if (res == ESP_OK) {
+            size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+            res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+        }
+        if (res == ESP_OK) {
+            res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+        }
+        if (res == ESP_OK) {
+            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+        }
+
+        // フレーム初期化
+        if (fb) {
+            esp_camera_fb_return(fb);
+            fb = NULL;
+            _jpg_buf = NULL;
+        } else if (_jpg_buf) {
+            free(_jpg_buf);
+            _jpg_buf = NULL;
+        }
+
+        // 送信失敗 -> 中断
+        if (res != ESP_OK) {
+            break;
+        }
+
+        // 現在のフレーム送信終了時刻
+        int64_t fr_end = esp_timer_get_time();
+
+        // 処理時間計算
+        int64_t frame_time = fr_end - last_frame; // 処理時間
+        last_frame = fr_end; // 前回のフレーム送信終了時刻更新
+        frame_time /= 1000; // 秒 -> マイクロ秒変換
+
+        // シリアル通信
+        Serial.printf("MJPG: %uB %ums (%.1ffps)\n", (uint32_t)(_jpg_buf_len),
+                        (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time);
+    }
+
+    // リソース解放
+    esp_camera_fb_return(fb);
+    free(_jpg_buf);
+
+    last_frame = 0; // 前回のフレーム送信終了時刻初期化
+    return res;
+}
+
+// カメラ映像設定
+void camera_setting()
+{
+    sensor_t *s = esp_camera_sensor_get();
+
+    s->set_framesize(s, (framesize_t)6);  // Resolution
+    s->set_quality(s, 10);        // Quality
+    s->set_brightness(s, 0);      // Brightness
+    s->set_contrast(s, 0);        // Contrast
+    s->set_saturation(s, 0);      // Saturation
+    s->set_special_effect(s, 0);  // Special effect
+    s->set_whitebal(s, 1);        // AWB
+    s->set_awb_gain(s, 1);        // AWB Gain
+    s->set_wb_mode(s, 0);         // WB mode
+    s->set_exposure_ctrl(s, 1);   // AEC Sensor
+    s->set_aec2(s, 0);            // AEC DSP
+    s->set_ae_level(s, 0);        // AE Level
+    s->set_aec_value(s, 1);       // Exposure
+    s->set_gain_ctrl(s, 1);       // AGC
+    s->set_agc_gain(s, 0);        // Gain
+    s->set_gainceiling(s, (gainceiling_t)0); // Gain ceiling
+    s->set_bpc(s, 0);             // BPC
+    s->set_wpc(s, 1);             // WPC
+    s->set_raw_gma(s, 1);         // Raw GMA
+    s->set_lenc(s, 1);            // Lens Correction
+    s->set_hmirror(s, 1);         // H-Mirror
+    s->set_dcw(s, 1);             // DCW (Downsize EN)
+    s->set_colorbar(s, 0);        // Color Bar
+}
+
+void startCameraServer()
+{
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    // カメラ映像設定
+    camera_setting();
+    // 人感センサー セットアップ
+    pinMode(AS312_PIN, INPUT);
+
+    // 画像取得 ハンドラ紐付け
+    httpd_uri_t index_uri = {
+        .uri       = "/",
+        .method    = HTTP_GET,
+        .handler   = index_handler,
+        .user_ctx  = NULL
+    };
+    Serial.printf("Starting web server on port: '%d'\n", config.server_port);
+    if (httpd_start(&camera_httpd, &config) == ESP_OK) {
+        httpd_register_uri_handler(camera_httpd, &index_uri);
+    }
+
+    // 映像取得 ハンドラ紐付け
+    httpd_uri_t stream_uri = {
+        .uri       = "/stream",
+        .method    = HTTP_GET,
+        .handler   = stream_handler,
+        .user_ctx  = NULL
+    };
+    config.server_port += 1;
+    config.ctrl_port += 1;
+    Serial.printf("Starting stream server on port: '%d'\n", config.server_port);
+    if (httpd_start(&stream_httpd, &config) == ESP_OK) {
+        httpd_register_uri_handler(stream_httpd, &stream_uri);
+    }
+}
---
+++ esp32/sketch/sketch.ino
@@ -0,0 +1,307 @@
+#include <WiFi.h> // WiFi関連のヘッダーファイル
+#include <WiFiUdp.h>  // UDP関連のヘッダーファイル
+#include <Wire.h> // I2C関連のヘッダーファイル
+#include <string.h>
+#include "esp_camera.h" // カメラ関連のヘッダーファイル
+
+#include "select_pins.h" // センサーやカメラなどのピン情報
+
+// アクセスポイントモード
+// #define SOFTAP_MODE
+#if defined(SOFTAP_MODE)
+#endif
+
+// 自動スリープ時使用 (今回は不要)
+#define uS_TO_S_FACTOR 1000000 // μS -> S変換
+#define TIME_TO_SLEEP  5 // 自動スリープになる時間(秒)
+
+// 無線LAN情報
+#define WIFI_SSID   "*************" // SSID
+#define WIFI_PASSWD "*************" // パスワード
+String macAddress = ""; // MACアドレス
+String ipAddress = "";  // IPアドレス
+
+// Raspberry Pi情報
+static WiFiUDP udp;
+static const char *raspip = "192.168.1.8";
+static const int raspport = 6100;
+
+int send_udp_flg = 0; // PIR反応フラグ 送信フラグ
+char udp_data[128]; // UDP受信データ
+
+extern void startCameraServer();
+
+// ボタン アクティブ
+#include <OneButton.h>
+OneButton button(BUTTON_1, true);
+
+// ディスプレイ アクティブ
+#include "SSD1306.h"
+#include "OLEDDisplayUi.h"
+#define SSD1306_ADDRESS 0x3c
+SSD1306 oled(SSD1306_ADDRESS, I2C_SDA, I2C_SCL, (OLEDDISPLAY_GEOMETRY)SSD130_MODLE_TYPE);
+OLEDDisplayUi ui(&oled);
+
+bool deviceProbe(uint8_t addr)
+{
+  Wire.beginTransmission(addr);
+  return Wire.endTransmission() == 0;
+}
+
+// ディスプレイ セットアップ
+bool setupDisplay()
+{
+  // セットアップ失敗 -> 中断
+  if (!deviceProbe(SSD1306_ADDRESS))return false;
+  
+  oled.init();
+  oled.setTextAlignment(TEXT_ALIGN_CENTER); // 中央揃え
+  oled.setFont(ArialMT_Plain_16);           // フォント
+
+  // 起動画面 表示
+  oled.drawString(oled.getWidth() / 2, oled.getHeight() / 2 - 16, "SmartLock");
+  oled.drawString(oled.getWidth() / 2, oled.getHeight() / 2, "System");
+  oled.display();
+
+  return true;
+}
+
+// ディスプレイ出力
+void smartlock()
+{
+  // PIR反応 -> 認証開始
+  while (digitalRead(AS312_PIN))
+  {
+    // ボタン反応 有効
+    button.tick();
+
+    // PIR反応フラグ送信
+    if (send_udp_flg == 0)
+    {
+      // PIR反応フラグをRaspに送信
+      udp.beginPacket(raspip, raspport);
+      udp.write('1');
+      udp.endPacket();
+      delay(500);
+
+      send_udp_flg = 1; // UDP送信フラグ True
+    }
+    
+    oled.clear();
+
+    oled.setTextAlignment(TEXT_ALIGN_CENTER); // 中央揃え
+    oled.setFont(ArialMT_Plain_16); // フォント
+
+    Serial.println("Authentication...");
+    oled.drawString(oled.getWidth() / 2, oled.getHeight() / 2 - 16, "Authentication");
+    oled.display();
+  }
+
+  oled.clear(); // 画面初期化
+
+  oled.setTextAlignment(TEXT_ALIGN_CENTER); // 中央揃え
+  oled.setFont(ArialMT_Plain_16); // フォント
+
+  oled.drawString(oled.getWidth() / 2, oled.getHeight() / 2 - 16, "SmartLock");
+  oled.drawString(oled.getWidth() / 2, oled.getHeight() / 2, "System");
+  oled.display();
+
+  send_udp_flg = 0; // UDP送信フラグ False
+}
+
+// 外部電源供給 セットアップ
+bool setupPower()
+{
+  #define IP5306_ADDR 0X75
+  #define IP5306_REG_SYS_CTL0 0x00
+  
+  // セットアップ失敗 -> 中断
+  if (!deviceProbe(IP5306_ADDR))return false;
+  
+  // セットアップ
+  bool en = true;
+  Wire.beginTransmission(IP5306_ADDR);
+  Wire.write(IP5306_REG_SYS_CTL0);
+  if (en)
+    Wire.write(0x37);
+  else
+    Wire.write(0x35);
+  return Wire.endTransmission() == 0;
+
+  return true;
+}
+
+// 人感センサー セットアップ
+bool setupSensor()
+{
+  pinMode(AS312_PIN, INPUT);
+  return true;
+}
+
+// カメラ セットアップ
+bool setupCamera()
+{
+  camera_config_t config;
+
+  #if defined(Y2_GPIO_NUM)
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_JPEG;
+    //init with high specs to pre-allocate larger buffers
+    if (psramFound()) {
+      config.frame_size = FRAMESIZE_UXGA;
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+    } else {
+      config.frame_size = FRAMESIZE_SVGA;
+      config.jpeg_quality = 12;
+      config.fb_count = 1;
+    }
+  #endif
+
+  // セットアップ失敗 -> 中断
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x\n", err);
+    return false;
+  }
+
+  sensor_t *s = esp_camera_sensor_get();
+  // 反転
+  s->set_hmirror(s, 1);
+  s->set_vflip(s, 1);
+
+  return true;
+}
+
+// ボタン セットアップ
+void setupButton()
+{
+  // 2回プッシュ -> スリープモード
+  button.attachDoubleClick([]() {    
+    ui.disableAutoTransition(); // スライドアニメーション無効
+
+    oled.setTextAlignment(TEXT_ALIGN_CENTER); // 中央揃え
+    oled.setFont(ArialMT_Plain_10); // フォント
+    oled.clear(); // 画面初期化
+
+    // スリープモード移行表示
+    Serial.println("Sleep mode");
+    oled.drawString(oled.getWidth() / 2 - 5, oled.getHeight() / 2 - 20, "Transition to sleep mode...");
+    oled.drawString(oled.getWidth() / 2, oled.getHeight() / 2, "Press the button");
+    oled.drawString(oled.getWidth() / 2, oled.getHeight() / 2 + 10, "to wake up.");
+    oled.display();
+    delay(3000);
+
+    // ディスプレイ出力OFF
+    oled.clear();
+    oled.displayOff();
+
+    // スリープモード移行
+    esp_sleep_enable_ext1_wakeup(((uint64_t)(((uint64_t)1) << BUTTON_1)), ESP_EXT1_WAKEUP_ALL_LOW); // 復帰条件
+    esp_deep_sleep_start();
+  });
+}
+
+// ネットワーク セットアップ
+void setupNetwork()
+{
+  macAddress = "LilyGo-CAM-";
+
+  // アクセスポイントモード
+  #ifdef SOFTAP_MODE
+    WiFi.mode(WIFI_AP);
+    macAddress += WiFi.softAPmacAddress().substring(0, 5);
+    WiFi.softAP(macAddress.c_str());
+    ipAddress = WiFi.softAPIP().toString();
+  #else // ステーションモード
+    WiFi.begin(WIFI_SSID, WIFI_PASSWD);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    ipAddress = WiFi.localIP().toString();
+    macAddress += WiFi.macAddress().substring(0, 5);
+  #endif
+}
+
+// UDP セットアップ
+void setupUDP()
+{
+  udp.begin(6000);
+}
+
+// セットアップ
+void setup()
+{
+  // シリアル通信開始 (115200bps)
+  Serial.begin(115200);
+  Wire.begin(I2C_SDA, I2C_SCL);
+
+  bool status;
+  // ディスプレイ セットアップ
+  status = setupDisplay();
+  Serial.print("setupDisplay status: "); Serial.println(status);
+
+  // 外部電源供給 セットアップ
+  status = setupPower();
+  Serial.print("setupPower status: "); Serial.println(status);
+
+  // 人感センサー セットアップ
+  status = setupSensor();
+  Serial.print("setupSensor status: "); Serial.println(status);
+
+  // カメラ セットアップ
+  status = setupCamera();
+  Serial.print("setupCamera status: "); Serial.println(status);
+
+  // セットアップ失敗 -> 再試行
+  if (!status) {
+    delay(10000);
+    esp_restart();
+  }
+
+  // ボタン セットアップ
+  setupButton();
+  // ネットワーク セットアップ
+  setupNetwork();
+  // UDP セットアップ
+  setupUDP();
+  // Start
+  startCameraServer();
+
+  // IPアドレス情報 シリアルモニター出力
+  Serial.print("IP Address: "); Serial.println(ipAddress);
+}
+int test_flg = 0;
+// ディスプレイ出力
+void loop()
+{
+  // 施錠フラグ Trueならば
+  if (udp.parsePacket()) {
+    udp.read(udp_data, 128);
+    if (strcmp(udp_data, "lock") == 0)
+    {
+      smartlock();
+    }
+  }
+}
