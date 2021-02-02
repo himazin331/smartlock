d95583524400b983a24379c3b89669d1fa0c5da8
himazin331 <sion20022013@gmail.com>
2021-02-03 08:20:11 +0900

    Get Camera Image

---
+++ getCameraImg.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import os
+import io
+import urllib
+from urllib.request import urlopen
+
+
+cnt = 0
+while True:
+    # カメラサーバー(ESP32)からカメラ画像を取得
+    file = io.BytesIO(urllib.request.urlopen('http://192.168.1.12').read())
+    frame = Image.open(file)
+
+    # 保存
+    frame.save(os.path.join('./img/', 'log'+str(cnt)+'.jpg'))
+    if cnt == 1200:
+        break
+    cnt += 1
