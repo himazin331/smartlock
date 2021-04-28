241d54a5ce053f4678f66d94b248b7df23395c41
himazin331 <sion20022013@gmail.com>
2021-04-28 20:15:10 +0900

    CGI

--- smartlk.py
+++ smartlk.py
@@ -4,20 +4,20 @@ import json
 import servo_key
 
 status = ""
-status = sys.stdin.readline() # POSTデータ
+status = sys.stdin.readline()  # POSTデータ
 
 k = servo_key.key()
-if status != "": # 施錠/解錠
+if status != "":  # 施錠/解錠
     print('Content-Type: text/html;')
-    print('Access-Control-Allow-Methods: POST;\nAccess-Control-Allow-Origin: https://YYY.himazin331.com\n')
+    print('Access-Control-Allow-Methods: POST;\nAccess-Control-Allow-Origin: https://smartlk.himazin331.com\n')
 
     if "0" in status:
         k.unlock()
     else:
         k.lock()
-else: # ステータス&ログ取得
+else:  # ステータス&ログ取得
     print('Content-Type: application/json;')
-    print('Access-Control-Allow-Methods: POST;\nAccess-Control-Allow-Origin: https://YYY.himazin331.com\n')
+    print('Access-Control-Allow-Methods: POST;\nAccess-Control-Allow-Origin: https://smartlk.himazin331.com\n')
 
     result = k.readlog()
     print(json.dumps(result))
